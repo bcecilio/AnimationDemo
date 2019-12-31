8456f2035d96601499a1d44ba22e30ab42e81b09
brendon cecilio <brendon.cecilio@gmail.com>
2019-12-31 16:59:30 -0500

    "API file added"

--- AnimationDemo/Network Services/QuoteAPI.swift
+++ AnimationDemo/Network Services/QuoteAPI.swift
@@ -8,4 +8,31 @@
 
 import Foundation
 
-
+struct QuoteAPI {
+    static func fetchQuote(completion: @escaping (Result<Quote,AppError>) -> ()) {
+        
+        let endpointURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let quoteData = try JSONDecoder().decode(Quote.self, from: data)
+                    let quote = quoteData
+                    completion(.success(quote))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
