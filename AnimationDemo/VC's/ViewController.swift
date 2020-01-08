929a08510223536e4079a1aebd2e7b56414dda6d
brendon cecilio <brendon.cecilio@gmail.com>
2020-01-08 13:52:23 -0500

    "image files added UI changed for both main and secondary VC's"

--- AnimationDemo/VC's/ViewController.swift
+++ AnimationDemo/VC's/ViewController.swift
@@ -12,29 +12,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var quoteView: UITextView!
     @IBOutlet weak var author: UILabel!
-    @IBOutlet weak var ronImage: UIImage!
     @IBOutlet weak var getEnlightenedButton: UIButton!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var cursorImage: UIImageView!
     
     var quote = [Quote]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getEnlightenedButton.layer.cornerRadius = 9
+        view.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        quoteView.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        yellowView.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
     }
     
-//    func loadData() {
-//        QuoteAPI.fetchQuote { (result) in
-//            switch result {
-//            case .failure(let appError):
-//                DispatchQueue.main.async {
-//                    self.showAlert(title: "App Error", message: "\(appError)")
-//                }
-//            case .success(let randomQuote):
-//                self.quote =
-//            }
-//        }
+//    func animateCursor() {
+//
+//        cursorImage.transform = CGAffineTransform(translationX: 2, y: 2)
+//        view.addSubview(cursorImage)
 //    }
     
     @IBAction func getQuoteButton(_ sender: UIButton) {
+        
+        quoteView.transform = CGAffineTransform(scaleX: 2, y: 2)
+        quoteView.transform = .identity
         
         QuoteAPI.fetchQuote { [weak self] (result) in
             switch result {
